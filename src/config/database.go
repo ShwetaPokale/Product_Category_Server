5698f267ea9c5c5843efc94c9aa1a813cb2666e5
Shiv Sarwade <shiv.s.sarwade@gmail.com>
2025-05-04 09:50:21 +0530

    added iamge part in product and updated category method a bit

--- src/config/database.go
+++ src/config/database.go
@@ -2,10 +2,14 @@ package config
 
 import (
 	"fmt"
+	"log"
 	"os"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+
+	"Product_Category_Server/src/models/product"
+	"Product_Category_Server/src/models/user"
 )
 
 type DBConfig struct {
@@ -21,7 +25,7 @@ func NewDBConfig() *DBConfig {
 		Host:     getEnv("DB_HOST", "localhost"),
 		Port:     getEnv("DB_PORT", "5432"),
 		User:     getEnv("DB_USER", "postgres"),
-		Password: getEnv("DB_PASSWORD", "post123"),
+		Password: getEnv("DB_PASSWORD", "root"),
 		DBName:   getEnv("DB_NAME", "my_database"),
 	}
 }
@@ -32,9 +36,30 @@ func (c *DBConfig) GetDSN() string {
 }
 
 func InitDB(config *DBConfig) (*gorm.DB, error) {
-	db, err := gorm.Open(postgres.Open(config.GetDSN()), &gorm.Config{})
+	dsn := config.GetDSN()
+	log.Printf("Connecting to database with DSN: %s", dsn)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		return nil, fmt.Errorf("error opening database: %v", err)
+	}
+
+	log.Println("Successfully connected to database")
+
+	// Auto-migrate the schema
+	err = db.AutoMigrate(&user.User{}, &product.Product{})
+	if err != nil {
+		return nil, fmt.Errorf("error migrating database: %v", err)
+	}
+
+	log.Println("Database migration completed")
+
+	// Test the connection by counting products
+	var count int64
+	if err := db.Model(&product.Product{}).Count(&count).Error; err != nil {
+		log.Printf("Error counting products: %v", err)
+	} else {
+		log.Printf("Current number of products in database: %d", count)
 	}
 
 	return db, nil
